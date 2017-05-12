e1b966bdae94f7496610083d0708bbfb76e7d9b1
Tatsunosuke Shimada <adam.is.t521@gmail.com>
2017-05-12 17:30:36 +0900

    more counselors in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,19 @@ User.create(email: "techcamp@tech.camp", password: "666666", password_confirmati
             avatar: open("http://res.cloudinary.com/dngqhhpqw/image/upload/v1494574888/22/original/gen.png"))
 
 Counselor.create(user_id: 2, name: "新垣結衣", response: "大丈夫、なんとかなるよー",
-                 avatar: open("http://res.cloudinary.com/dngqhhpqw/image/upload/v1494575000/23/original/gakki.jpg"))
+                 avatar: open("http://res.cloudinary.com/dngqhhpqw/image/upload/v1494577636/gakki_vzn5q1.jpg"))
+
+Counselor.create(user_id: 2, name: "トランプ", response: "大丈夫、偉大だから！",
+                 avatar: open("http://res.cloudinary.com/dngqhhpqw/image/upload/v1494577635/trump_dyauog.jpg"))
+
+Counselor.create(user_id: 2, name: "小島よしお", response: "ダイジョブダイジョブー",
+                 avatar: open("http://res.cloudinary.com/dngqhhpqw/image/upload/v1494577636/kojima_vdpias.png"))
+
+Counselor.create(user_id: 2, name: "ワンガリ・マータイ", response: "モッタイナイ。",
+                 avatar: open("http://res.cloudinary.com/dngqhhpqw/image/upload/v1494577635/maathai_rtupwt.jpg"))
+
+Counselor.create(user_id: 2, name: "てるふぃー", response: "サッカーチームのほうだよ。",
+                 avatar: open("http://res.cloudinary.com/dngqhhpqw/image/upload/v1494577635/terufi_um7hak.jpg"))
+
 
 Trouble.create(user_id: 2, counselor_id: 2, content: "どうしたらいいかなぁ。助けて!")
